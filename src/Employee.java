bfe2513cb0c7bbabb189c49456bbb35348239a05
Ekaterina <klarina2409@gmail.com>
2023-09-06 23:18:46 +0300

    курсовая

--- src/Employee.java
+++ src/Employee.java
@@ -1,46 +1,59 @@
 public class Employee {
-        private String fullName;
-        private int department;
-        private double salary;
-        private static int counter = 0;
-        private int id;
+    private String fullName;
+    private int department;
+    private double salary;
+    private static int counter = 0;
+    private int id;
 
-        public Employee(String fullName, int department, double salary) {
-            this.fullName = fullName;
-            this.department = department;
-            this.salary = salary;
-            this.id =counter++;
+    public Employee(String fullName, int department, double salary) {
+        this.fullName = fullName;
+        this.department = department;
+        this.salary = salary;
+        this.id = counter++;
 
-        }
-        public int getId() {
-            return id;
-        }
-
-        public String getFullName() {
-            return this.fullName;
-        }
-
-        public int getDepartment (){
-            return this.department;
-        }
-
-        public double getSalary (){
-            return this.salary;
-        }
-
-        public void SetDepartment (int department){
-            this.department = department;
-        }
-
-        public void SetSalary ( double salary) {
-            this.salary = salary;
-        }
-
-        public static void getAllEmployees  (Employee[] employees) {
-        for (Employee employee : employees) {
-            System.out.println(employee.toString());
-        }
-
-
-        }
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return this.fullName;
+    }
+
+    public int getDepartment() {
+        return this.department;
+    }
+
+    public double getSalary() {
+        return this.salary;
+    }
+
+    public void SetDepartment(int department) {
+        this.department = department;
+    }
+
+    public void SetSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        final String s = "Employee " +
+                "fullName='" + fullName + '\'' + ", " +
+                "department=" + department +
+                ", salary=" + salary +
+                ", id=" + id;
+        return s;
+    }
+
+    public static void getAllEmployees(Employee[] employees) {
+        for (Employee employee : employees) {
+            if (employee != null) {
+                System.out.println(employee.toString());
+            }
+        }
+
+
+            }
+        }
--- src/Main.java
+++ src/Main.java
@@ -30,15 +30,90 @@ public class Main {
         System.out.println(employee4.getSalary());
         System.out.println(employee5.getSalary());
 
-        Employee [] employees = new Employee[10];
+        Employee[] employees = new Employee[10];
 
-        employees [0] = new Employee("Inna Breduk", 1, 1000);
-        employees [1] = new Employee("Katy Larina", 2, 2000);
-        employees [2] = new Employee("Mary Rodionova", 3, 3000);
-        employees [3] = new Employee("Vita Kosenko", 4, 4000);
-        employees [4] = new Employee("Luba Kievec", 5, 5000);
+        employees[0] = new Employee("Inna Breduk", 1, 1000);
+        employees[1] = new Employee("Katy Larina", 2, 2000);
+        employees[2] = new Employee("Mary Rodionova", 3, 3000);
+        employees[3] = new Employee("Vita Kosenko", 4, 4000);
+        employees[4] = new Employee("Luba Kievec", 5, 5000);
+
 
         Employee.getAllEmployees(employees);
 
+        double totalSalaryExpense = calculateMonthlySalaryExpense(employees);
+        System.out.println("Сумма затрат на зарплаты в месяц: " + totalSalaryExpense);
+
+        findEmployeeWithMinSalary(employees);
+
+        findEmployeeWithMaxSalary(employees);
+
+        double averageSalary = calculateAverageSalary(employees);
+        System.out.println("Средняя зарплата: " + averageSalary);
+
+        printEmployeesFullNames(employees);
+
     }
-}
+
+    public static double calculateMonthlySalaryExpense(Employee[] employees) {
+        double totalSalaryExpense = 0;
+        for (Employee employee : employees) {
+            if (employee != null) {
+                totalSalaryExpense += employee.getSalary();
+            }
+        }
+        return totalSalaryExpense;
+
+    }
+
+
+    public static void findEmployeeWithMinSalary(Employee[] employees) {
+        double minSalary = employees[0].getSalary();
+        for (Employee employee : employees) {
+            if (employee != null && employee.getSalary() < minSalary) {
+                minSalary = employee.getSalary();
+            }
+        }
+        for (Employee employee : employees) {
+            if (employee != null && employee.getSalary() == minSalary) {
+                System.out.println("Сотрудник с минимальной зарплатой " + employee.toString());
+            }
+        }
+    }
+
+        public static void findEmployeeWithMaxSalary(Employee[] employees) {
+            double minSalary = employees[0].getSalary();
+            for (Employee employee : employees) {
+                if (employee != null && employee.getSalary() > minSalary) {
+                    minSalary = employee.getSalary();
+                }
+            }
+            for (Employee employee : employees) {
+                if (employee != null && employee.getSalary() == minSalary) {
+                    System.out.println("Сотрудник с максимальной зарплатой " + employee.toString());
+                }
+            }
+        }
+
+    public static double calculateAverageSalary(Employee[] employees) {
+        double totalSalaryExpense = calculateMonthlySalaryExpense(employees);
+        int numEmployees = 0;
+        for (Employee employee : employees) {
+            if (employee != null) {
+                numEmployees++;
+            }
+        }
+        double averageSalary = totalSalaryExpense / numEmployees;
+        return averageSalary;
+    }
+
+    public static void printEmployeesFullNames(Employee[] employees) {
+        for (Employee employee : employees) {
+            if (employee != null) {
+                System.out.println(employee.getFullName());
+            }
+        }
+
+    }
+
+    }
